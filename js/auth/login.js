c4e9d19ffc210f02ce7c39c63dc00244056a2486
Vikash Kumar <vikashisexploringai@gmail.com>
2026-03-22 00:14:44 +0530

    many things

--- js/auth/login.js
+++ js/auth/login.js
@@ -54,6 +54,8 @@ function renderLogin() {
     updateBottomNav('login');
 }
 
+
+
 async function handleLogin() {
     const email = document.getElementById('email')?.value;
     const password = document.getElementById('password')?.value;
